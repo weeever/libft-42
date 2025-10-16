558ebb13083840c0fa9e84195e72b6c22b736dbf
weeever <weeever@Weeever.localdomain>
2025-10-16 22:15:29 +0200

    fix memmove et strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tidebonl <tidebonl@student.42.fr>          +#+  +:+       +#+        */
+/*   By: weeever <weeever@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 17:54:56 by tidebonl          #+#    #+#             */
-/*   Updated: 2025/10/16 18:19:21 by tidebonl         ###   ########.fr       */
+/*   Updated: 2025/10/16 22:14:39 by weeever          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	result = malloc(sizeof(char *) * (strlen(s1) + strlen(s2)) + 1);
+	result = malloc(sizeof(char) * (strlen(s1) + strlen(s2)) + 1);
 	if (!result)
 		return (NULL);
 	while (s1[i] != '\0')
