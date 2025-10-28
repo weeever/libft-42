ca073f8d7a8d44ad0c1cf5482aec536da4ef7206
weeever <weeever@Weeever.localdomain>
2025-10-28 12:20:18 +0100

    final2

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tidebonl <tidebonl@student.42.fr>          +#+  +:+       +#+        */
+/*   By: weeever <weeever@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 10:58:28 by tidebonl          #+#    #+#             */
-/*   Updated: 2025/10/21 12:44:51 by tidebonl         ###   ########.fr       */
+/*   Updated: 2025/10/28 12:20:06 by weeever          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (lst)
+	if (lst && new)
 	{
 		new->next = *lst;
 		*lst = new;
